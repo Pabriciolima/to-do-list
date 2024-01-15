44f8ea361df0ac5d6987dc9bca4cf0417a5195e8
pabricio lima <pabricio.dev.1@gmail.com>
2024-01-15 12:00:47 -0300

    adicionado botão para adição de tarefas

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function btn(){
 
-        const pegarValor = document.getElementById('text');
-        const valor = pegarValor.value
+        const pegarValor = document.getElementById('text').value;
+        
+        var novaTarefa = document.createElement("li");
+        novaTarefa.textContent = pegarValor;
 
-        console.log(valor)
-}
+        document.getElementById("listaDeTarefas").appendChild(novaTarefa);
+
+        document.getElementById("pegarValor").value = "";
+
+        document.getElementById("valorNaTela").innerHTML = "Tarefa: " + pegarValor;
+        }
+
 
