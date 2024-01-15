a2c3cc1529dd36259b7f85ecc3bf7e261e384875
pabricio lima <pabricio.dev.1@gmail.com>
2024-01-15 13:49:11 -0300

    adicionado botão de lixeira

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function btn(){
 
         const pegarValor = document.getElementById('text').value;
-        
         var novaTarefa = document.createElement("li");
         novaTarefa.textContent = pegarValor;
 
+
+        var botaoLixeira = document.createElement("button");
+        novaTarefa.appendChild(botaoLixeira);
+        botaoLixeira.innerHTML = "&#128465;";
+        botaoLixeira.onclick = function() {
+        novaTarefa.remove();
+        };
+
+        
+
         document.getElementById("listaDeTarefas").appendChild(novaTarefa);
-
-        document.getElementById("pegarValor").value = "";
-
+        document.getElementById("text").value = "";
         document.getElementById("valorNaTela").innerHTML = "Tarefa: " + pegarValor;
         }
 
